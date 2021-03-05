d528ceb59924c671ba24643aad557ba8cb577a23
Maxim270693 <maxim.zhuk.93@mail.ru>
2021-03-05 22:50:42 +0300

    '111'

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {value01_01} from "./01/01";
+import {value01_01} from "./01-hello-tests/01";
 
 let a = value01_01
 
